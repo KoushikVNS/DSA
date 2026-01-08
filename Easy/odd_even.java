87baa516070979d65e11079417c895d257f7d311
Kaushik Chakraborty <125153524+KoushikVNS@users.noreply.github.com>
2026-01-08 19:41:23 +0530

    Update odd_even.java

--- Easy/odd_even.java
+++ Easy/odd_even.java
@@ -1,6 +1,6 @@
-/*In a programming contest, you get a question: determine whether a given number is prime. You remember a prime number is a number greater
-    than 1 that has no divisors other than 1 and itself. To solve this, you decide to check divisibility from 2 up to the square root of the number.
-    This way, you can quickly find if any divisor exists and decide primality.*/
+/*One day, while teaching your little sibling about numbers, you wonder how a computer can tell if a number is even or odd. 
+You recall that even numbers are divisible by 2 without a remainder, and odd numbers are not. 
+To make it clear, you decide to write a simple program that checks the divisibility by 2 to classify any given number.*/
 
 import java.util.*;
 public class Main {
