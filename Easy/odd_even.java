45ca4655894078ae14b898a44332afc3c765bcd2
Kaushik Chakraborty <125153524+KoushikVNS@users.noreply.github.com>
2026-01-08 19:40:31 +0530

    Update odd_even.java
    Added a comment explaining the prime number determination logic.

--- Easy/odd_even.java
+++ Easy/odd_even.java
@@ -1,3 +1,7 @@
+/*In a programming contest, you get a question: determine whether a given number is prime. You remember a prime number is a number greater
+    than 1 that has no divisors other than 1 and itself. To solve this, you decide to check divisibility from 2 up to the square root of the number.
+    This way, you can quickly find if any divisor exists and decide primality.*/
+
 import java.util.*;
 public class Main {
     public static void main(String[] args) {
